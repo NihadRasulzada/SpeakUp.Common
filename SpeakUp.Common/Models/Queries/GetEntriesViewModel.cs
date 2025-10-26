df1d25585a71dc9d7470a8c63505d42ff629cb55
NihadRasulzada <dev.nihad.rasulzada@outlook.com>
2025-10-26 22:24:10 +0400

    Add command and query models for entry and user details

---
+++ SpeakUp.Common/Models/Queries/GetEntriesViewModel.cs
@@ -0,0 +1,10 @@
+namespace SpeakUp.Common.Models.Queries;
+
+public class GetEntriesViewModel
+{
+    public Guid Id { get; set; }
+    public string Subject { get; set; }
+
+    public int CommentCount { get; set; }
+
+}
